3c3ace298a9f647734d96054850d3be7d0c19ca3
Aidil <aidilrozi83@gmail.com>
2020-08-12 00:14:53 -0500

    Created a dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+	return (
+		<div className='app'>
+			<div className='app__header'>
+				<h1>COVID 19 tracker</h1>
+				<FormControl className='app__dropdown'>
+					<Select variant='outlined' value='abc'>
+						{/* Loop through all the countries and show the dropdown */}
+
+						{countries.map((country) => (
+							<MenuItem value='country'>{country}</MenuItem>
+						))}
+					</Select>
+				</FormControl>
+			</div>
+
+			{/* Header */}
+			{/* Title + Select input dropdown field */}
+
+			{/* InfoBoxes */}
+			{/* InfoBoxes */}
+			{/* InfoBoxes */}
+
+			{/* Table */}
+			{/* Graph */}
+
+			{/* Map */}
+		</div>
+	);
 }
 
 export default App;
