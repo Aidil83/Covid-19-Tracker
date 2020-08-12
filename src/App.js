c5d17444ff4be0253d101e6f602b6f027e081877
Aidil <aidilrozi83@gmail.com>
2020-08-12 10:46:00 -0500

    Grabbed all the countries with an API call and made the 'worldwide' to be displayed on the dropdown menu by default

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { useState } from 'react';
 import './App.css';
 import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useEffect } from 'react';
 
 function App() {
-	const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']);
+	const [countries, setCountries] = useState([]);
+	const [country, setCountry] = useState('worldwide');
+
+	useEffect(() => {
+		const getCountriesData = async () =>
+			fetch('https://disease.sh/v3/covid-19/countries')
+				.then((response) => response.json())
+				.then((data) => {
+					const countries = data.map((country) => ({
+						name: country.country,
+						value: country.countryInfo.iso2,
+					}));
+					setCountries(countries);
+				});
+		getCountriesData();
+	}, []);
 	return (
 		<div className='app'>
 			<div className='app__header'>
 				<h1>COVID 19 tracker</h1>
 				<FormControl className='app__dropdown'>
-					<Select variant='outlined' value='abc'>
+					<Select variant='outlined' value={country}>
 						{/* Loop through all the countries and show the dropdown */}
-
+						<MenuItem value='worldwide'>Worldwide</MenuItem>
 						{countries.map((country) => (
-							<MenuItem value='country'>{country}</MenuItem>
+							<MenuItem value={country.value}>{country.name}</MenuItem>
 						))}
 					</Select>
 				</FormControl>
